184624167ba186eab67dd236f7c3a502e050fce5
Ocen Chris <chrisocen4@gmail.com>
2022-04-10 20:16:28 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Chrx Post Cars
+ A WordPress plugin with a custom post type called Cars storing data about cars
---
+++ public/partials/chrx-post-cars-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       http://ocenchris.com
+ * @since      1.0.0
+ *
+ * @package    Chrx_Post_Cars
+ * @subpackage Chrx_Post_Cars/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
